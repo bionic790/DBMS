92b6dc01138211d5306e553a538da1d8a477a978
bionic790 <53811382+bionic790@users.noreply.github.com>
2020-05-13 12:22:46 +0530

    Update BANK.SQL

--- BANK.SQL
+++ BANK.SQL
@@ -38,56 +38,48 @@ account_no int,
 foreign key(customer_name) references depositor(customer_name),
 foreign key(account_no) references accounts(account_no)
 );
-insert into branch values('branch_name12','branch_city1',100000),
-('branch_name22','branch_city2',200000),
-('branch_name32','branch_city3',300000);
+insert into branch values('branch_name12','branch_city1',100000);
+insert into branch values('branch_name22','branch_city2',200000);
+insert into branch values('branch_name32','branch_city3',300000);
 select * from branch;
-insert into accounts values
-(11111,'branch_name12',111),
-(22222,'branch_name12',222),
-(33333,'branch_name22',333),
-(44444,'branch_name22',444),
-(55555,'branch_name32',555),
-(66666,'branch_name32',666);
-insert into accounts values
-(11112,'branch_name12',111),
-(11113,'branch_name12',111)
-;
+insert into accounts values(11111,'branch_name12',111);
+insert into accounts values(22222,'branch_name12',222);
+insert into accounts values(33333,'branch_name22',333);
+insert into accounts values(44444,'branch_name22',444);
+insert into accounts values(55555,'branch_name32',555);
+insert into accounts values(66666,'branch_name32',666);
+insert into accounts values(11112,'branch_name12',111);
+insert into accounts values(11113,'branch_name12',111);
 select * from accounts;
-insert into depositor values
-('cust1','street1','city1'),
-('cust2','street2','city2'),
-('cust3','street3','city3'),
-('cust4','street4','city4'),
-('cust5','street5','city5');
+insert into depositor values('cust1','street1','city1');
+insert into depositor values('cust2','street2','city2');
+insert into depositor values('cust3','street3','city3');
+insert into depositor values('cust4','street4','city4');
+insert into depositor values('cust5','street5','city5');
 select * from depositor;
-insert into Loan values
-(11,'branch_name12',120000),
-(12,'branch_name12',120000),
-(21,'branch_name22',220000),
-(22,'branch_name22',220000),
-(31,'branch_name32',320000),
-(32,'branch_name32',320000);
+insert into Loan values(11,'branch_name12',120000);
+insert into Loan values(12,'branch_name12',120000);
+insert into Loan values(21,'branch_name22',220000);
+insert into Loan values(22,'branch_name22',220000);
+insert into Loan values(31,'branch_name32',320000);
+insert into Loan values(32,'branch_name32',320000);
 select * from loan;
-insert into borrower values
-('cust1',11),
-('cust2',12),
-('cust3',21),
-('cust4',22),
-('cust5',31);
+insert into borrower values('cust1',11);
+insert into borrower values('cust2',12);
+insert into borrower values('cust3',21);
+insert into borrower values('cust4',22);
+insert into borrower values('cust5',31);
 select * from borrower;
 select * from depositor ;
-insert into customer values
-('cust1',11111),
-('cust2',22222),
-('cust3',33333),
-('cust4',44444),
-('cust5',55555);
-insert into customer values
-('cust1',11112),
-('cust1',11113);
+insert into customer values('cust1',11111);
+insert into customer values('cust2',22222);
+insert into customer values('cust3',33333);
+insert into customer values('cust4',44444);
+insert into customer values('cust5',55555);
+insert into customer values('cust1',11112);
+insert into customer values('cust1',11113);
 select * from customer;
 select (c.customer_name) from customer c,Accounts a
 where a.account_no=c.account_no and a.branch_name= 'branch_name12' group by c.customer_name having count(*)>=2;
 select (c.customer_name) from customer c,Accounts a,branch b
-where a.account_no=c.account_no and a.branch_city= 'branch_city1' 
+where a.account_no=c.account_no and b.branch_city = 'branch_city1' ;
